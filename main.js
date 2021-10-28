d0821c3a99393303344be16d413845eba65e2360
chuongtang <chuong.tang@aol.com>
2021-10-28 17:50:42 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+<div class="dropdown">
+<button class="dropbtn">Dropdown</button>
+<div class="dropdown-content">
+  <a href="adaUSD.html">Cardano</a>
+  <a href="bnbUSD.html">Binance-Coin</a>
+  <a href="btcUSD.html">BitCoin</a>
+  <a href="dogeUSD.html">DogeCoin</a>
+  <a href="dotUSD.html">Polkadot-New</a>
+  <a href="ethUSD.html">Ethereum</a>
+  <a href="shibUSD.html">Shiba-Inu</a>
+  <a href="solUSD.html">Solana</a>
+  <a href="xrpUSD.html">XRP</a>
+  
+</div>
+</div> 
+`
